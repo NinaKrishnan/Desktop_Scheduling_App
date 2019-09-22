cbdcf3c6228f72022715474e3b794f7e38c30f7a
NinaKrishnan <48103731+NinaKrishnan@users.noreply.github.com>
2019-09-22 08:52:07 -0400

    Added comments and cleaned up formatting

--- src/com/nkris/scheduling_app/controllers/helpers/EventPopUpController.java
+++ src/com/nkris/scheduling_app/controllers/helpers/EventPopUpController.java
@@ -1,5 +1,6 @@
 package com.nkris.scheduling_app.controllers.helpers;
 
+//imports
 import java.net.URL;
 import java.sql.SQLException;
 import java.util.ResourceBundle;
@@ -9,7 +10,6 @@ import com.jfoenix.controls.JFXTimePicker;
 import com.nkris.scheduling_app.controllers.EventController;
 import com.nkris.scheduling_app.database.SQL_Appointments;
 import com.nkris.scheduling_app.models.Appointment;
-import com.nkris.scheduling_app.models.Customer;
 
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -18,12 +18,41 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+
+
+
+
+//*****************************************************************************************************************//
+//****************************************************************************************************************//
+
+
+
+/*
+ * This is the controller class for the appointment creator popup window. When a calendar day is clicked (on the 
+ * dashboard), a popup window is displayed with the current agenda for that day, as well as a button for creating a 
+ * new appointment. When "new appointment" button is clicked, an appointment creator popup takes the place of the 
+ * initial popup window. This popup contains fields for user to enter required information about the appointment
+ * being created: title, start date & time, end date & time, description, customer, contact info, location, and type.
+ * When the "save" button is clicked, after ensuring all fields have been adequately filled, a new appointment object
+ * is created with the information provided by the user. The appointment is then added to the appropriate day on the
+ * dashboard calendar, and saved to the MySQL database.
+ */
+
+
+
+//*****************************************************************************************************************//
+//****************************************************************************************************************//
+
+
 
 public class EventPopUpController implements Initializable
 {
@@ -31,45 +60,47 @@ public class EventPopUpController implements Initializable
 
 	
 	@FXML
-	private Button saveEventButton;
+	private Button saveEventButton; //Button to create and save new event and add it to DB
 		
 	@FXML
-	private Button cancelEventButton;
+	private Button cancelEventButton; //Button to cancel creating a new event
 	
 	@FXML
-	private  TextField eventTitleTextField;
+	private  TextField eventTitleTextField; //Textfield for appointment title (String)
 	
 	@FXML
-	private  JFXDatePicker startDatePicker;
+	private  JFXDatePicker startDatePicker; //Date Picker for appointment start date (LocalDate)
 	
 	@FXML
-	private  JFXDatePicker endDatePicker;
+	private  JFXDatePicker endDatePicker; //Date Picker for appointment end date (LocalDate)
 	
 	@FXML
-	private  JFXTimePicker startTimePicker;
+	private  JFXTimePicker startTimePicker; //Time Picker for appointment start time (LocalTime)
 	
 	@FXML
-	private  JFXTimePicker endTimePicker;
+	private  JFXTimePicker endTimePicker; //Time Picker for appointment end time (LocalTime)
 	
 	@FXML
-	private  TextArea descriptionTextArea;
+	private  TextArea descriptionTextArea; //Textfield for appointment description (String)
 	
 	@FXML
-	private TextField customerTextField;
+	private TextField customerTextField; //Textfield for appointment customer; brings up a table of customers in 
+										//DB to select from. Textfield is filled with customer's name. (Customer)
 	
 	@FXML
-	private TextField contactTextField;
+	private TextField contactTextField; //Textfield for customer's contact info for appointment (String)
 	
 	@FXML
-	private TextField locationTextField;
+	private TextField locationTextField; //Textfield for appointment location (String)
 	
 	@FXML
-	private TextField typeTextField;
+	private TextField typeTextField; //Textfield for appointment type (String)
 	
 	
-	public static Appointment currentEvent;
+	public static Appointment currentEvent; //When this is non-null, it means an appointment has just been created.
+											//The DashboardController class will use this to add a flag to the
+											//calendar on the right day, then sets this back to null. 
 	
-	public static ObservableList<Appointment> eventContainer;
 	
 	
 	
@@ -78,7 +109,12 @@ public class EventPopUpController implements Initializable
 	{
 		
 	}
- 
+	
+	
+ /*
+  * This instantiates a new appointment with the information the user entered in the textfield. The appointment 
+  * is then added to the database.
+  */
 	@FXML
 	public void saveEvent(ActionEvent event)
 	{
@@ -107,7 +143,12 @@ public class EventPopUpController implements Initializable
 	}
 	
 	
-	
+
+	/*
+	 * When the customer textfield is clicked, a list of customers in the database comes up that the user can 
+	 * select from. A customer not in the database must first be added before they can be selected for an 
+	 * appointment.
+	 */
 	@FXML
 	private void showCustomerSelection(MouseEvent event)
 	{
@@ -141,18 +182,33 @@ public class EventPopUpController implements Initializable
 	}
 
 	
+	//Return the current event 
 	public static Appointment getEvent()
 	{
 		return currentEvent;
 	}
 	
+	//Cancel adding new event - get confirmation then close the window
 	@FXML
 	private void cancelNewEvent(ActionEvent event)
 	{
-		
+		if(confirmCancelNewEvent()) {
+		    ((Stage)(((Button)event.getSource()).getScene().getWindow())).close();
+		}
 	}
 	
-
+	//Ask for confirmation before cancelling new event
+	private boolean confirmCancelNewEvent()
+	{
+		Alert alert = new Alert(AlertType.CONFIRMATION, 
+				"If you cancel, the event will not be saved to your calendar. Do you want to continue?",
+				ButtonType.YES, ButtonType.NO);
+		alert.showAndWait();
+		if(alert.getResult() == ButtonType.YES) {
+			return true;
+		}
+		return false;
+	}
 	
 	
 
