f7f3f8194d1be659cb8166da13a904fef8c98c6a
NinaKrishnan <48103731+NinaKrishnan@users.noreply.github.com>
2019-09-22 08:24:44 -0400

    Removed unused tests

--- src/com/nkris/scheduling_app/unit_tests/CalendarTests.java
+++ src/com/nkris/scheduling_app/unit_tests/CalendarTests.java
@@ -2,19 +2,15 @@ package com.nkris.scheduling_app.unit_tests;
 
 import static org.junit.Assert.assertEquals;
 
-import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
 import com.nkris.scheduling_app.controllers.helpers.ViewCustomerController;
 import com.nkris.scheduling_app.database.DatabaseHandler;
 import com.nkris.scheduling_app.database.SQL_Address;
-import com.nkris.scheduling_app.database.SQL_Customer;
-import com.nkris.scheduling_app.models.Address;
-import com.nkris.scheduling_app.models.City;
-import com.nkris.scheduling_app.models.Country;
-import com.nkris.scheduling_app.models.Customer;
 
 class CalendarTests {
 
@@ -57,13 +53,7 @@ class CalendarTests {
 	
 	
 	
-	@Test
-	void getCustomerID() throws SQLException, ClassNotFoundException
-	{
-		DatabaseHandler.connect();
-		Connection conn = DatabaseHandler.getDBconnection();
-		assertEquals(SQL_Customer.getLastIndex(conn), 0);
-	}
+	
 	
 	@Test
 	void getId() throws SQLException, ClassNotFoundException
@@ -71,6 +61,10 @@ class CalendarTests {
 		DatabaseHandler.connect();
 		assertEquals(SQL_Address.getAddressId(1), 1);
 	}
+	
+	
+	
+	
 	
 	
 }
